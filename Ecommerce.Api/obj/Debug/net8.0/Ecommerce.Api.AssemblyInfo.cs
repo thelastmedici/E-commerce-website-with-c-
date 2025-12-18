262e0b23d27d9aaebc089d1f90e03d8afd49e538
thelastmedici <christianopeyemijoshua@gmail.com>
2025-12-18 11:14:04 +0000

    added both user and product mode

--- Ecommerce.Api/obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ Ecommerce.Api/obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61dd26c43da13613400c5335b44e1d54dc2660e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57112d24d516472b31dbda2e013b82b71dbdd24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
